458b398ae91433bd1ce4cbb54eeb0f56df3a0b7f
Bhagyashree <bhagyashreemarde09@gmail.com>
2022-04-27 04:46:45 +0530

    displaying map, current location done

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, View, Dimensions, Alert, Button } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
+  let [currentLocation, setCurrentLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [errorMsg, setErrormsg] = useState();
+
+  const clickHandler = () => {
+    //function to handle click on floating Action Button
+    Alert.alert("Floating Button Clicked");
+  };
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrormsg("Permission denied, cannot access current location.");
+        alert(errorMsg);
+        return;
+      }
+
+      currentLocation = await Location.getCurrentPositionAsync({});
+      setCurrentLocation({
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+      });
+    })();
+  }, []);
+
+  console.log(currentLocation.latitude, currentLocation.longitude);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        showsUserLocation
+        region={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("./assets/current_location.png")}
+        />
+      </MapView>
     </View>
   );
 }
@@ -13,8 +61,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  touchableOpacityStyle: {
+    position: "absolute",
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    right: 30,
+    bottom: 30,
+  },
+  floatingButtonStyle: {
+    resizeMode: "contain",
+    width: 50,
+    height: 50,
+    //backgroundColor:'black'
   },
 });
